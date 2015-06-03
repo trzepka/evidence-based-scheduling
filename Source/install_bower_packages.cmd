257cf4bd6cfcac9827550d256373e8e4bff970f6
trzepka <tomasz_rz@poczta.fm>
2015-06-03 23:34:10 +0100

    Integrated bower packages setup into VS build pipeline

--- Source/install_bower_packages.cmd
+++ Source/install_bower_packages.cmd
@@ -1,8 +1,9 @@
 pushd
-cd ..\Source\EvidenceBasedScheduling\Content
+cd EvidenceBasedScheduling\Content
 call bower install d3
 call bower install angular
 call bower install angular-resource
 call bower install angular-route
 call bower install bootstrap
 popd
+exit /b 0
