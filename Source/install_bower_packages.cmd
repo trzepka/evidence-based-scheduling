3c3ecaee8320fe01bb19a73c64092e868890c9ff
trzepka <tomasz_rz@poczta.fm>
2015-06-07 08:23:17 +0100

    updated bower managed modules
    changed installation directory
    created bower.json
    updated install_bower_packages.cmd

--- Source/install_bower_packages.cmd
+++ Source/install_bower_packages.cmd
@@ -1,9 +1,5 @@
 pushd
-cd EvidenceBasedScheduling\Content
-call bower install d3
-call bower install angular
-call bower install angular-resource
-call bower install angular-route
-call bower install bootstrap
+cd EvidenceBasedScheduling
+call bower install
 popd
 exit /b 0
