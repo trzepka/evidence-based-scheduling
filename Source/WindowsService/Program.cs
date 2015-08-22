afde0ae39bd2c6b40fb6cd889d7e2ec96226d0f1
trzepka <tomasz_rz@poczta.fm>
2015-08-22 19:30:08 +0100

    Added an initial skeleton of a WindowsService
    Based on Topshelf library

---
+++ Source/WindowsService/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace EvidenceBasedScheduling
+{
+    class Program
+    {
+        public static void Main()
+        {
+            HostFactory.Run(x =>                                 
+            {
+                x.Service<SchedulingService>();
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Evidence Based Scheduling Service");
+                x.SetDisplayName("Evidence Based Scheduling Service");
+                x.SetServiceName("EBS-Service");
+            });   
+        }
+    }
+}
---
+++ Source/WindowsService/SchedulingService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Topshelf;
+
+namespace EvidenceBasedScheduling
+{
+    public class SchedulingService: ServiceControl
+    {
+        public bool Start(HostControl hostControl)
+        {
+            Console.WriteLine("started");
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Console.WriteLine("stopped");
+            return true;
+        }
+    }
+}
